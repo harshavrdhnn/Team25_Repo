4977371ea42b58dc55b1f6a0b30ab337e1d1fa7c
harsha <harsha@DESKTOP-CIJGPU2>
2022-07-13 13:37:41 +0530

    addded account

--- SystemInformationSystem/src/model/Account.java
+++ SystemInformationSystem/src/model/Account.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Account {
+	
+	
+			int accno;
+			int mobnum;
+			public int getAccno() {
+				return accno;
+			}
+			public void setAccno(int accno) {
+				this.accno = accno;
+			}
+			public int getMobnum() {
+				return mobnum;
+			}
+			public void setMobnum(int mobnum) {
+				this.mobnum = mobnum;
+			}
+			
 
 }
