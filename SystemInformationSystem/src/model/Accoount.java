f9457219d9358bb4bdd869fd8470c34958fa2885
harsha <harsha@DESKTOP-CIJGPU2>
2022-07-13 13:31:07 +0530

    account

---
+++ SystemInformationSystem/src/model/Accoount.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Accoount {
+		
+		int accountNo;
+		double balance;
+		public int getAccountNo() {
+			return accountNo;
+		}
+		public void setAccountNo(int accountNo) {
+			this.accountNo = accountNo;
+		}
+		public double getBalance() {
+			return balance;
+		}
+		public void setBalance(double balance) {
+			this.balance = balance;
+		}
+		
+		
+}
